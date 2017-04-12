09777d0a69943e56f3b54d55bdbb89e2484d9f05
Jo Taylor <jotaylor@stsci.edu>
2017-04-12 14:13:14 -0400

    Initial commit

---
+++ install_branch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# get current working directory
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# first and second input arguments
+direc=$1
+branch=$2
+
+# go to git directory and checkout branch
+cd $direc
+git checkout $branch
+git pull
+
+# install calcos
+rm -rf build/
+rm lib/calcos/version.py
+export PYTHONPATH="${direc}/install/lib/python"
+export lref="/grp/hst/cdbs/lref/"
+python ./setup.py build
+python ./setup.py install --home=${direc}/install
+
+# Return to original directory
+cd $cwd
